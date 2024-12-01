ccc89279865e689154270f3f88ce848a7b81a55c
mk749 <kumarmanojdoppalapudi@gmail.com>
2024-12-01 02:07:26 -0500

    Test 4

--- function_app.py
+++ function_app.py
@@ -20,6 +20,6 @@ def func_testinggit(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "Hey I automate the pipeline process.. I wANT TO TRY THIS AGAIN YAR",
+             "Hey I automate the pipeline process.. I wANT TO TEST",
              status_code=200
         )
